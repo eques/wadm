a84e4da98ae7f1aefd2650101d4c10bbc44d2580
Karina Pilyushonoka <k.pilyushonoka@gmail.com>
2016-11-26 01:19:16 +0200

    links for side menu

--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -3,7 +3,10 @@ import { Component } from "@angular/core";
 @Component({
   selector: "main-menu",
   template: `
-      left side menu.
-`
+    <dl>
+    <dd><a href="/#/programms">{{"main-menu.link-all_programms" | translate}}</a></dd>
+    <dd><a href="/#/activate-fidebox">{{"main-menu.link-activate_fidebox" | translate}}</a></dd>
+    </dl> 
+  `
 })
 export class MainMenuComponent {}
