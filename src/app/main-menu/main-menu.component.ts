a0541011804cfea16cda64ec11b916c15bfb3608
YourLittleHelper <oskars@shibetec.com>
2016-11-26 05:08:37 +0200

    Add more stylings for side menu.

--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -5,8 +5,8 @@ import { Component } from "@angular/core";
   template: `
 
     <ul class="nav nav-pills nav-stacked">
-        <li><a href="/#/programms"><span class="glyphicon glyphicon-list"></span> {{"main-menu.link-all_programms" | translate}}</a></li>
-        <li><a href="/#/activate-fidebox"><span class="glyphicon glyphicon-off"></span>  {{"main-menu.link-activate_fidebox" | translate}}</a></li>
+        <li [routerLink]="['/programms']" [routerLinkActive]="['active']"><a href="/#/programms"><span class="glyphicon glyphicon-list"></span> {{"main-menu.link-all_programms" | translate}}</a></li>
+        <li [routerLink]="['/activate-fidebox']" [routerLinkActive]="['active']"><a href="/#/activate-fidebox"><span class="glyphicon glyphicon-off"></span>  {{"main-menu.link-activate_fidebox" | translate}}</a></li>
     </ul>
   `
 })
