96baacdc799bc23277b651e348959d264ecc01f8
YourLittleHelper <oskars@shibetec.com>
2016-11-27 10:29:10 +0200

    Add small fix.

--- bs-config.dev.js
+++ bs-config.dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "port": process.env.BS_PORT || 9101,
+  "port": process.env.BS_PORT || 80,
   "proxy": "http://localhost:" + (process.env.EXPRESS_PORT || 3000),
   "ui": { "port": process.env.BS_PORT_UI || 9102 },
   "files": [
